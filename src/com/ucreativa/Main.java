e7f771dc28252e758c9b286b393d21ea59bd2ce7
Juan Cordoba <wivercr@gmail.com>
2021-05-24 19:47:19 -0600

    Cree la clase Juan

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -2,12 +2,13 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Juan;
+import com.ucreativa.familia.Papas;
 
 public class Main {
 
     public static void main(String[] args) {
         Juan Hola = new Juan(); //instancio la clase
-
+        Papas Padres = new Papas("Hola Hijo.");
 
     }
 }
---
+++ src/com/ucreativa/familia/Papas.java
@@ -0,0 +1,8 @@
+package com.ucreativa.familia;
+
+public class Papas {
+
+    public Papas(String Saludo){
+        System.out.println(Saludo);
+    }
+}
