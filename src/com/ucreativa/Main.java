3f07648b6a7f0298edbd9a06d9d2bf1a34140c3b
Juan Cordoba <wivercr@gmail.com>
2021-05-24 20:52:12 -0600

    Creando Getters, Setters

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -9,6 +9,12 @@ public class Main {
     public static void main(String[] args) {
         Juan Hola = new Juan(); //instancio la clase
         Papas Padres = new Papas("Hola Hijo.");
+        int Edad = Padres.getAge();
+        String Pasatiempo = Padres.getHobby();
+
+        System.out.println("Edad: " + Edad);
+        System.out.println("Hobby : " + Pasatiempo);
+
 
     }
 }
--- src/com/ucreativa/familia/Papas.java
+++ src/com/ucreativa/familia/Papas.java
@@ -2,7 +2,24 @@ package com.ucreativa.familia;
 
 public class Papas {
 
+    private int Age;
+    private String Hobby;
+
+    public void setAge(){
+        this.Age = this.Age + 1;
+    }
+
+    public int getAge(){
+        return this.Age;
+    }
+
+    public String getHobby(){
+        return this.Hobby;
+    }
+
     public Papas(String saludo){
+        this.Age = 79;
+        this.Hobby = "Coser";
         System.out.println(saludo);
     }
 }
