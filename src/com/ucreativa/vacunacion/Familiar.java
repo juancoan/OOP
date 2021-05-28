fcd551b718863eebe1de4e1d888ff7473e7791b8
Juan Cordoba <wivercr@gmail.com>
2021-05-27 20:59:55 -0600

    Creando paquete vacunacion

---
+++ src/com/ucreativa/vacunacion/Familiar.java
@@ -0,0 +1,20 @@
+package com.ucreativa.vacunacion;
+
+public class Familiar extends Persona{
+
+    private String parentesco;
+
+    public Familiar(String nombre, String cedula, int edad, Boolean riesgo, String parentesco) {
+        super(nombre, cedula, edad, riesgo);
+        this.parentesco = parentesco;
+    }
+
+    public String getParentesco() {
+        return parentesco;
+    }
+
+
+
+
+
+}
---
+++ src/com/ucreativa/vacunacion/Persona.java
@@ -0,0 +1,45 @@
+package com.ucreativa.vacunacion;
+
+public class Persona {
+
+    private String nombre;
+    private String cedula;
+
+
+
+    public Persona(String nombre, String cedula, int edad, Boolean riesgo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.riesgo = riesgo;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setRiesgo(Boolean riesgo) {
+        this.riesgo = riesgo;
+    }
+
+    private int edad;
+    private Boolean riesgo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public Boolean getRiesgo() {
+        return riesgo;
+    }
+
+
+}
